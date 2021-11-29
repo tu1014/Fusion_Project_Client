2422947efea706235db5de4819d023df89a3d0c2
tu1014 <tu1014@naver.com>
2021-11-30 06:57:26 +0900

    교과목 다이나믹 조회

--- src/main/java/adminController/SubjectListController.java
+++ src/main/java/adminController/SubjectListController.java
@@ -2,47 +2,157 @@ package adminController;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import network.Connector;
+import network.Protocol;
+import persistence.Entity.Student;
+import persistence.Entity.Subject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SubjectListController implements Initializable {
 
-    @FXML ComboBox<String> gradeBox;
+    @FXML ComboBox<String> filter;
     @FXML TextField keyWord;
     @FXML AnchorPane panel;
+    @FXML VBox listBox;
 
     AdminMainController parentController;
 
     void setParentController(AdminMainController con) { parentController = con; }
 
-    int searchGrade = 0;
     String searchKeyWord = "";
+    InputStream is;
+    OutputStream os;
+    Protocol protocol;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        gradeBox.getItems().add("grade");
-        gradeBox.getItems().add("subject name");
-        gradeBox.getItems().add("subject code");
-        gradeBox.setOnAction(this::setSearchGrade);
+
+        protocol = Connector.getProtocol();
+        Socket socket = Connector.getSocket();
+
+        try {
+            is = socket.getInputStream();
+            os = socket.getOutputStream();
+        }
+
+        catch (IOException e) { e.printStackTrace(); }
+
+        filter.getItems().add("No Filter");
+        filter.getItems().add("grade");
+        filter.getItems().add("subject name");
+        filter.getItems().add("subject code");
+        filter.setOnAction(this::setKeyWord);
     }
 
-    public void setSearchGrade(ActionEvent event) {
+    String subjectCode = "";
+    String subjectName = "";
+    int grade = 0;
 
-        String choice = gradeBox.getValue();
+    private void initKey() {
+        subjectCode = "";
+        subjectName = "";
+        grade = 0;
+    }
 
-        if (choice.equals("1학년")) searchGrade = 1;
-        else if (choice.equals("2학년")) searchGrade = 2;
-        else if (choice.equals("3학년")) searchGrade = 3;
-        else if (choice.equals("4학년")) searchGrade = 4;
-        else searchGrade = 0;
+    public void setKeyWord(ActionEvent event) {
 
-        parentController.showMessage("검색 키워드 : " + choice);
+        keyWord.setText("");
+        String choice = filter.getValue();
+        searchKeyWord = choice;
+        parentController.showMessage("검색 필터 : " + choice);
 
+    }
+
+    @FXML
+    private void search() {
+
+        listBox.getChildren().clear();
+
+        protocol.init();
+        protocol.setHeader(Protocol.REQUEST, Protocol.READ, Protocol.SUBJECT);
+
+        String input = keyWord.getText();
+
+        initKey();
+
+        if (searchKeyWord.equals("subject code")) {
+            subjectCode = '%' + input + '%';
+        }
+
+        if (searchKeyWord.equals("subject name")) {
+            subjectName = '%' + input + '%';
+        }
+
+        if (searchKeyWord.equals("grade")) {
+            grade = Integer.parseInt(input);;
+        }
+
+        protocol.addBodyStringData(subjectCode.getBytes());
+        protocol.addBodyStringData(subjectName.getBytes());
+        protocol.addBodyIntData(grade);
+
+        try {
+            os.write(protocol.getPacket());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Connector.read();
+
+        byte[] header = Connector.getHeader();
+        if(header[Protocol.INDEX_CODE] == Protocol.FAIL) {
+            parentController.showMessage("교과목이 존재하지 않습니다.");
+            // return;
+        }
+
+        else {
+
+            parentController.showMessage("교과목 정보를 로드하였습니다");
+            readSubject();
+
+        }
+
+
+    }
+
+    public void readSubject() {
+
+        int count = Connector.readInt();
+
+        for(int i=0; i<count; i++) {
+            int id = Connector.readInt();
+            String code = Connector.readString();
+            String name = Connector.readString();
+            int grade = Connector.readInt();
+            int semester = Connector.readInt();
+            int credit = Connector.readInt();
+
+            Subject subject = new Subject(id, code, name, grade, semester, credit);
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../fxml/admin/subjectListItem.fxml"));
+            VBox item = null;
+
+            try { item = fxmlLoader.load(); }
+            catch (IOException e) { e.printStackTrace(); }
+
+            SubjectListItemController con = fxmlLoader.getController();
+            con.setSubjectListController(this);
+            con.setSubject(subject);
+            con.setText();
+            listBox.getChildren().add(item);
+        }
     }
 
 
